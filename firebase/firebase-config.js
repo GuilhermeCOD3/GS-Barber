0f6f8f1873f3f341fffa6dae55e7fb472a1aba43
Guilherme <guideoliveira369@gmail.com>
2026-07-16 23:15:55 -0300

    Adicionado o firebase com o database do sql

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,12 +1,11 @@
-// Configuração do Firebase para futura integração.
-// Substitua os valores abaixo pelas suas credenciais reais do projeto Firebase.
+// Configuração do Firebase do projeto GS Barber.
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "SEU_ID",
-    appId: "SEU_APP_ID"
+    apiKey: "AIzaSyA86H3pTQi61v5YJBjbr_1GTZGgo3bQgxs",
+    authDomain: "gs-barber-13d23.firebaseapp.com",
+    projectId: "gs-barber-13d23",
+    storageBucket: "gs-barber-13d23.firebasestorage.app",
+    messagingSenderId: "406752535638",
+    appId: "1:406752535638:web:363c17978b5345f97047f7"
 };
 
 window.firebaseConfig = firebaseConfig;
